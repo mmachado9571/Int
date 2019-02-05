beec994daae95938d96f56906665aadf49afccbf
mmachado9571 <mmachado9571@H202R1S7.STUDENT.ad.fgcu.edu>
2019-02-05 16:18:56 -0500

    1

---
+++ Int/src/Main.java
@@ -0,0 +1,28 @@
+// Marcos Machado
+//
+
+// Types of Data and Ranges:
+// Byte: -128 to 127
+// Short: -32,768 to 32,767
+// Integer: -2^31 to 2^31 - 1
+// Long: -2^63 to 2^63 - 1
+// Float: Range is beyond the scope.
+// Double: Range is beyond the scope.
+// Boolean: True or False values.
+// Character: 0 to 65,535
+// String: Non-numerical values (special support type).
+
+// Control+Shift+F automatically formats the code.
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Check out this crazy project.");
+
+//1
+    int int1 = 2;
+    double double1 = 2.0;
+    String string1 = "2";
+  }
+}
+ 
